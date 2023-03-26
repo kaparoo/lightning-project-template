40e6d7fa210c7f23957ec0a949b7d21d3a95ccb7
Jaewoo Park <kaparoo2001@gmail.com>
2023-03-26 09:55:00 +0900

    ✅ Update and pass test cases for `FashionMNISTClassifier`

--- tests/models/test_dummy.py
+++ tests/models/test_dummy.py
@@ -5,22 +5,25 @@ import random
 import pytest
 import torch
 
-from project.models.dummy import MNISTClassifier
+from project.models.dummy import FashionMNISTClassifier
 
 
 @pytest.fixture()
 def device():
     return "cuda" if torch.cuda.is_available() else "cpu"
 
+
 @pytest.fixture()
 def random_input(device):
     batch_size = random.randint(1, 64)
     return torch.randn(batch_size, 1, 28, 28, device=device)
 
+
 def test_init(device):
-    MNISTClassifier().to(device)
+    FashionMNISTClassifier().to(device)
+
 
 def test_forward(device, random_input):
-    model = MNISTClassifier().to(device)
+    model = FashionMNISTClassifier().to(device)
     y_hat = model(random_input)
     assert y_hat.shape == torch.Size([len(random_input), model.hparams.out_channels])
