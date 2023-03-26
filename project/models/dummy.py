8621152e137ec05055a10aaa7edf23d0f82a430b
Jaewoo Park <kaparoo2001@gmail.com>
2023-03-26 10:21:17 +0900

    ✨ Update `FashionMNISTClassifier`

--- project/models/dummy.py
+++ project/models/dummy.py
@@ -2,7 +2,7 @@
 
 __all__ = ("FashionMNISTClassifier",)
 
-
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from lightning import LightningModule
@@ -12,12 +12,12 @@ from torch.optim import Adam
 
 class FashionMNISTClassifier(LightningModule):
     def __init__(
-        self, 
-        in_channels: int = 1, 
-        out_channels: int = 10, 
-        learning_rate: float = 0.0002, 
-        beta1: float=0.5, 
-        beta2: float=0.999
+        self,
+        in_channels: int = 1,
+        out_channels: int = 10,
+        learning_rate: float = 0.0002,
+        beta1: float = 0.5,
+        beta2: float = 0.999,
     ) -> None:
         super().__init__()
         self.save_hyperparameters()
@@ -34,17 +34,33 @@ class FashionMNISTClassifier(LightningModule):
             nn.Linear(6400, out_channels),
             nn.Softmax(dim=1),
         )
+        self.num_classes = out_channels
+
+        self.step_losses: list[float] = []
+        self.epoch_losees: list[float] = []
 
     def forward(self, x: Tensor) -> Tensor:
         return self.network(x)
 
-    def training_step(self, batch: tuple[Tensor, Tensor], _: int):
-        images, labels = batch
-        preds = self(images)
-        return F.binary_cross_entropy(preds, labels)
-    
     def configure_optimizers(self):
         lr = self.hparams.learning_rate
         beta1 = self.hparams.beta1
         beta2 = self.hparams.beta2
         return Adam(self.network.parameters(), lr=lr, betas=(beta1, beta2))
+
+    def training_step(self, batch: tuple[Tensor, Tensor], _: int) -> Tensor:
+        images, labels = batch
+        preds = self(images)
+        labels = F.one_hot(labels, num_classes=self.num_classes)
+        labels = labels.type(torch.FloatTensor).to(self.device)
+        loss = F.binary_cross_entropy(preds, labels)
+        self.step_losses.append(loss.item())
+        self.log("loss", loss, prog_bar=True)
+        return loss
+
+    def on_train_epoch_start(self) -> None:
+        self.step_losses = []
+
+    def on_train_epoch_end(self) -> None:
+        epoch_loss = sum(self.step_losses) / len(self.step_losses)
+        self.epoch_losees.append(epoch_loss)
