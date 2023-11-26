b8f441f22a88da93963ed24ecf38247f7c67a231
Jaewoo Park <kaparoo2001@gmail.com>
2023-11-26 21:59:13 +0900

    🚨 Fix `ruff` warnings

--- project/models/dummy.py
+++ project/models/dummy.py
@@ -1,18 +1,16 @@
-# -*- coding: utf-8 -*-
-
 __all__ = ("FashionMNISTClassifier",)
 
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 from lightning import LightningModule
-from torch import Tensor
-from torch.optim import Adam
+from torch import Tensor, nn
+from torch.optim import Adam, Optimizer
+from typing_extensions import Self
 
 
 class FashionMNISTClassifier(LightningModule):
     def __init__(
-        self,
+        self: Self,
         in_channels: int = 1,
         out_channels: int = 10,
         learning_rate: float = 0.0002,
@@ -42,55 +40,52 @@ class FashionMNISTClassifier(LightningModule):
         self.valid_step_losses: list[float] = []
         self.valid_epoch_losees: list[float] = []
 
-    def forward(self, x: Tensor) -> Tensor:
-        return self.network.forward(x)  # type: ignore
+        self.lr = learning_rate
+        self.betas = (beta1, beta2)
 
-    def configure_optimizers(self):
-        lr = self.hparams.learning_rate
-        beta1 = self.hparams.beta1
-        beta2 = self.hparams.beta2
-        return Adam(self.network.parameters(), lr=lr, betas=(beta1, beta2))
+    def forward(self: Self, x: Tensor) -> Tensor:
+        return self.network.forward(x)  # type: ignore[no-any-return]
 
-    def parse_batch(self, batch: tuple[Tensor, Tensor]) -> tuple[Tensor, Tensor]:
+    def configure_optimizers(self: Self) -> Optimizer:
+        return Adam(self.network.parameters(), lr=self.lr, betas=self.betas)
+
+    def parse_batch(self: Self, batch: tuple[Tensor, Tensor]) -> tuple[Tensor, Tensor]:
         images, labels = batch
         labels = F.one_hot(labels, num_classes=self.num_classes)
         labels = labels.type(torch.FloatTensor).to(self.device)
         return images, labels
-    
-    def calc_loss(self, batch: tuple[Tensor, Tensor]) -> Tensor:
+
+    def calc_loss(self: Self, batch: tuple[Tensor, Tensor]) -> Tensor:
         images, labels = self.parse_batch(batch)
-        return F.binary_cross_entropy(self(images), labels)
+        return F.binary_cross_entropy(self.forward(images), labels)
 
-
-    def training_step(self, batch: tuple[Tensor, Tensor], _: int) -> Tensor:
+    def training_step(self: Self, batch: tuple[Tensor, Tensor], _: int) -> Tensor:
         loss = self.calc_loss(batch)
         self.train_step_losses.append(loss.item())
         self.log("train_loss", loss, prog_bar=True)
         return loss
 
-    def on_train_epoch_start(self) -> None:
+    def on_train_epoch_start(self: Self) -> None:
         self.train_step_losses = []
 
-    def on_train_epoch_end(self) -> None:
+    def on_train_epoch_end(self: Self) -> None:
         epoch_loss = sum(self.train_step_losses) / len(self.train_step_losses)
         self.train_epoch_losees.append(epoch_loss)
 
-    def validation_step(self, batch: tuple[Tensor, Tensor], _: int) -> Tensor:
+    def validation_step(self: Self, batch: tuple[Tensor, Tensor], _: int) -> Tensor:
         loss = self.calc_loss(batch)
         self.valid_step_losses.append(loss.item())
         self.log("val_loss", loss, prog_bar=True)
         return loss
 
-    def on_validation_epoch_start(self) -> None:
+    def on_validation_epoch_start(self: Self) -> None:
         self.valid_step_losses = []
 
-    def on_validation_epoch_end(self) -> None:
+    def on_validation_epoch_end(self: Self) -> None:
         epoch_loss = sum(self.valid_step_losses) / len(self.valid_step_losses)
         self.valid_epoch_losees.append(epoch_loss)
 
-    def test_step(self,  batch: tuple[Tensor, Tensor], _: int) -> Tensor:
+    def test_step(self: Self, batch: tuple[Tensor, Tensor], _: int) -> Tensor:
         loss = self.calc_loss(batch)
         self.log("test_loss", loss, prog_bar=True)
         return loss
-
-
